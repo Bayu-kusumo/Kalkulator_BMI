7e2f10b68734c9d87a3222fcf12408fb1716b92b
Bayu-kusumo <139141252+Bayu-kusumo@users.noreply.github.com>
2023-07-20 07:06:43 +0700

    fourth update.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-function hitung(bmi){
+document.addEventListener("DOMContentLoaded",function(event){
     var urlParams = new URLSearchParams(window.location.search);
     var berat = parseInt(urlParams.get("berat"));
     var tinggi = parseInt(urlParams.get("tinggi"));
@@ -18,21 +18,21 @@ function hitung(bmi){
         document.getElementById("kondisi").innerHTML = "Obesitas" ;
         document.getElementById("comment").innerHTML = "Anda Berada dalam Kategori Obesitas";
         document.getElementById("interpretasi").innerHTML = "Hasil BMI lebih dari 30";
-        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori kegemukan atau Obesitas. Usahakan untuk menurunkan berat badan dan menerapkan pola hidup sehat dengan menjaga makan dan aktivitas fisik.";
+        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori kegemukan atau Obesitas.";
     } else if (bmi > 29.9){
         document.getElementById("kondisi").innerHTML = "kelebihan Berat Badan";
         document.getElementById("comment").innerHTML = "Anda memiliki berat badan Berlebih";
         document.getElementById("interpretasi").innerHTML = "Hasil BMI diantara 25.0 hingga 29.9";
-        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori Overweight atau kelebihan berat badan. Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalor makanan yang dikonsumsi dan berolahraga.";
+        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori Overweight atau kelebihan berat badan.";
     } else if(bmi > 24.9){
         document.getElementById("kondisi").innerHTML = "Normal (Ideal)";
         document.getElementById("comment").innerHTML = "Anda memiliki Berat badan Yang Ideal";
         document.getElementById("interpretasi").innerHTML = "Hasil BMI diantara 18.5 hingga 24.9"
-        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori Ideal atau Normal. etap pertahankan berat badan Anda dan jaga berat badan Anda dengan mengatur keseimbangan antara pola makan dan aktivitas fisik Anda.";
+        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori Ideal atau Normal.";
     } else {
         document.getElementById("kondisi").innerHTML = "Kekurangan berat Badan";
         document.getElementById("comment").innerHTML = "Anda kekurangan berat badan";
         document.getElementsById("interpretasi").innerHTML = "Hasil BMI Kurang dari 18.5";
-        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori kategori kekurangan berat badan. Hubungi dokter lebih lanjut mengenai pola makan dan gizi yang baik untuk meningkatkan kesehatan.";
+        document.getElementById("kategori").innerHTML = "Anda berada dalam kategori kategori kekurangan berat badan.";
     }
 }
